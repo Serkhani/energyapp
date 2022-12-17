7aba9fd1c31e6f813783064cb7f605b50d497c67
Serkhani <dak.boniface@gmail.com>
2022-12-17 13:07:54 +0000

    added signInSection view

--- lib/app/data/theme.dart
+++ lib/app/data/theme.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData localTheme = ThemeData(
     useMaterial3: true,
-    scaffoldBackgroundColor: Colors.blue,
-
+    scaffoldBackgroundColor: const Color(0xff00008b),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 35.0
+      )
+    )
   );
 }
--- lib/app/modules/sign_in/views/signin_section_view.dart
+++ lib/app/modules/sign_in/views/signin_section_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 import 'package:get/get.dart';
 
 class SignInSectionView extends GetView {
@@ -7,7 +7,39 @@ class SignInSectionView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('SignIn')
+      body: SizedBox.expand(
+        child: CustomPaint(
+          painter: CirclePainter(),
+          willChange: false,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: Get.width * 0.1,
+              top: Get.height * 0.15),
+            child: Text('Sign In',
+            style: Theme.of(context).textTheme.titleLarge,),
+          ),
+        ),
+      ),
     );
   }
+}
+
+class CirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.white24;
+    Rect rect = Rect.fromCircle(
+        center: Offset(size.width * 0.8, 0.0), radius: size.width * 0.2);
+    Rect rect2 = Rect.fromCircle(
+        center: Offset(size.width * 0.9, size.height * 0.1),
+        radius: size.width * 0.2);
+    canvas.drawArc(rect, 0.0, math.pi, true, paint);
+    canvas.drawArc(rect2, 0.0, math.pi * 2, true, paint);
+  }
+
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(CirclePainter oldDelegate) => false;
 }
